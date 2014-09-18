dccbd2f91e192ecda2686acced6fb4313144ac8a
johnyzed <zenoujohn@gmail.com>
2014-09-18 14:01:37 +0300

    first commit

---
+++ puppet/modules/carbon/manifests/init.pp
@@ -0,0 +1,67 @@
+class carbon(
+
+$version="0.9.10",
+$build_dir = "/tmp"
+ 
+  ) {
+
+ $carbon_url = "http://launchpad.net/graphite/0.9/${version}/+download/carbon-${version}.tar.gz"
+ $carbon_loc = "$build_dir/carbon.tar.gz"
+
+ #include graphite
+
+  package { "python-twisted" :
+    ensure => latest
+  }
+
+ file { "/etc/init.d/carbon" :
+   source => "puppet:///modules/carbon/carbon",
+   ensure => present,
+ }
+
+ file { "/opt/graphite/conf/carbon.conf" :
+   source => "puppet:///modules/carbon/carbon.conf",
+   ensure => present,
+   notify => Service[carbon],
+   subscribe => Exec[install-carbon],
+ }
+
+ file { "/opt/graphite/conf/storage-schemas.conf" :
+   source => "puppet:///modules/carbon/storage-schemas.conf",
+   ensure => present,
+   notify => Service[carbon],
+   subscribe => Exec[install-carbon],
+ }
+
+ file { "/var/log/carbon" :
+   ensure => directory,
+   owner => www-data,
+   group => www-data,
+ }
+
+ exec { "download-graphite-carbon":
+   command => "wget -O $carbon_loc $carbon_url",
+   creates => "$carbon_loc"
+ }
+
+ exec { "unpack-carbon":
+   command => "tar -zxvf $carbon_loc",
+   cwd => $build_dir,
+   subscribe => Exec[download-graphite-carbon],
+   refreshonly => true,
+ }
+
+ exec { "install-carbon" :
+   command => "python setup.py install",
+   cwd => "$build_dir/carbon-$version",
+   require => Exec[unpack-carbon],
+   creates => "/opt/graphite/bin/carbon-cache.py",
+  }
+
+  service { "carbon":
+    enable => true,
+    ensure => running,
+    require=> Exec['install-carbon'],
+    }
+
+}
---
+++ puppet/modules/statsd/manifests/init.pp
@@ -0,0 +1,77 @@
+class statsd (
+
+$version="0.7.2",
+$build_dir = "/opt/statsd",
+
+	){
+$webapp_url="https://github.com/etsy/statsd/archive/v${version}.tar.gz"
+$webapp_loc = "$build_dir/graphite-web.tar.gz"
+$statsd_fullpath= "${build_dir}/statsd-${version}"
+
+   package { "nodejs" :
+     ensure => "present"
+   }
+
+   file { $build_dir:
+   	ensure => directory,
+   }
+
+  exec { "download-statsd":
+        command => "wget -O $webapp_loc $webapp_url",
+        creates => "$webapp_loc",
+        require => File[$build_dir],
+   }
+
+   exec { "unpack-statsd":
+     command => "tar -zxvf $webapp_loc",
+     cwd => $build_dir,
+     subscribe=> Exec[download-statsd],
+     refreshonly => true,
+     creates => $statsd_fullpath
+   }
+
+   file { "localConfig":
+   	path=> "$statsd_fullpath/localConfig.js",
+   	ensure => file,
+   	source => "puppet:///modules/statsd/localConfig.js"
+   }
+
+   file { "/etc/init/statsd.conf":
+    require => File['localConfig'],
+   	ensure => file,
+   	content => "
+#!upstart
+description \"Statsd node.js server\"
+author      \"Nicolas\"
+
+start on startup
+stop on shutdown
+
+script
+    export HOME=\"/root\"
+
+    echo $$ > /var/run/statsd.pid
+    exec sudo -u www-data /usr/bin/node ${statsd_fullpath}/stats.js ${statsd_fullpath}/localConfig.js  >> /var/log/statsd.log 2> /var/log/statsd.error.log
+end script
+
+pre-start script
+    # Date format same as (new Date()).toISOString() for consistency
+    echo \"[`date -u +%Y-%m-%dT%T.%3NZ`] (sys) Starting\" >> /var/log/statsd.log
+end script
+
+pre-stop script
+    rm /var/run/statsd.pid
+    echo \"[`date -u +%Y-%m-%dT%T.%3NZ`] (sys) Stopping\" >> /var/log/statsd.log
+end script
+   	",
+   	alias => "statsd_init",
+   }
+
+   service { "statsd":
+    enable => true,
+   	ensure => running,
+   	require => [Package['nodejs'],Exec['unpack-statsd'],File['statsd_init']]
+   }
+
+
+}
